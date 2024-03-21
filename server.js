7bec947dcc3a267812436da40d499133dc2735b8
Sabine Matthies <Sabine.Matthies@student.htw-berlin.de>
2024-03-21 10:12:15 +0100

    Aktualisiere server.js - Behebe Fehler in der Express-Konfiguration

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express(); // Express-App erstellen
 const PORT = 4000; 
 
 app.use(cors());
-app.use(express.json()); /
+app.use(express.json()); 
 
 const swaggerOptions = {
     definition: {
